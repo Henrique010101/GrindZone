0310a57e66227e741330e86a6bf2eaba57ca89e1
Henrique Guimarães <henrique2003.com@gmail.com>
2024-09-17 22:34:45 -0300

    rotas do cart

--- js/server/cart.js
+++ js/server/cart.js
@@ -63,3 +63,64 @@ async function fetchCart() {
         alert('erro ao obter itens do carrinho')
     }
 }
+
+function displayCartItems(cart) {
+    const cartContainer = document.getElementById('');
+    cartContainer.innerHTML = '';
+
+    if (cart.items.length === 0) {
+        cartContainer.innerHTML = <h2>Seu carrinho está vazio.</h2>;
+        return;
+    }
+
+    cart.items.forEach(item => {
+        const product = item.productId;
+        const cartItem = document.createElement('div');
+        cartItem.classList.add('cart-item');
+
+        cartItem.innerHTML = `
+        <article style="max-height: 120px;" class="d-flex py-1 border rounded cart-item">
+          <img class="image-cart" src="/assets/shape-maple-cbgang.jpg" alt="shape">
+          <div class="d-flex justify-content-between w-100 p-1">
+            <div class="d-flex descricao-itens-cart flex-column">
+              <h5 class="product-cart-name fw-semibold">Shape CB Gang</h5>
+                <p class="descricao-item-cart fs-6 fw-light"></p>
+            </div>
+            <div id="valor-item" class="d-flex flex-column align-items-center justify-content-end">
+              <span id="product-cart-price-promo" class="product-cart-price-promo fw-light text-decoration-line-through text-black-50">200,00 R$</span>
+              <span id="product-cart-price" class="product-cart-price">200,00 R$</span>
+            </div>
+          </div>
+        </article>
+        `
+
+        cartContainer.appendChild(cartItem);
+    });
+    const removeButtons = document.querySelectorAll('.remove-btn');
+    removeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const productId = this.getAttribute('data-id');
+            removeFromCart(productId);
+        });
+    });
+}
+
+async function updateCartDisplay() {
+    try {
+        const response = await fetch('http://localhost:3000/api/cart', {
+            method: 'GET',
+            credentials: 'include',
+        });
+
+        if(response.ok) {
+            const cart = await response.json();
+            displayCartItems(cary);
+        } else {
+            console.error('Erro ao atualizar o  carrinho:', response.statusText);
+            alert('Erro ao atualizar o carrinho.');
+        }
+    } catch(error) {
+        console.error('Error ao buscar itens do carrinho:', error);
+        alert('Erro ao buscar itens do carrinho.');
+    }
+}
